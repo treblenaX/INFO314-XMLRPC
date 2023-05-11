616599a1ffd31455cf8e12455a6b5929131bbfb4
treblenaX <elbertkcheng@outlook.com>
2023-05-10 21:15:54 -0700

    finished

--- JavaClient/Client.java
+++ JavaClient/Client.java
@@ -9,6 +9,7 @@ import javax.xml.transform.stream.*;
 import org.w3c.dom.*;
 
 import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 
 /**
  * This approach uses the java.net.http.HttpClient classes, which
@@ -17,8 +18,13 @@ import org.w3c.dom.Node;
 public class Client {
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
     private static HttpClient httpClient = HttpClient.newHttpClient();
+    private static String hostname;
+    private static String port;
 
     public static void main(String... args) throws Exception {
+        hostname = args[0];
+        port = args[1];
+
         System.out.println(add() == 0);
         System.out.println(add(1, 2, 3, 4, 5) == 15);
         System.out.println(add(2, 4) == 6);
@@ -27,40 +33,111 @@ public class Client {
         System.out.println(multiply(1, 2, 3, 4, 5) == 120);
         System.out.println(divide(10, 5) == 2);
         System.out.println(modulo(10, 5) == 0);
+        // errors
+        try {   // addition overflow
+            System.out.println(add(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {   // divide by zero
+            System.out.println(modulo(10, 0));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {   // multiplication overflow
+            System.out.println(multiply(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {   // divide by zero
+            System.out.println(divide(0, 0));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            System.out.println(subtract("1", "2"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static int add(int lhs, int rhs) throws Exception {
-        String xml = buildRequestXML("add", lhs, rhs);
-        return handleRequest(xml);
+        try {
+            String xml = buildRequestXML("add", lhs, rhs);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
     }
-    public static int add(Integer... params) throws Exception {
-        String xml = buildRequestXML("add", params);
-        return handleRequest(xml);
+    public static int add(Object... params) throws Exception {
+        try {
+            String xml = buildRequestXML("add", params);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
     }
     public static int subtract(int lhs, int rhs) throws Exception {
-        String xml = buildRequestXML("subtract", lhs, rhs);
-        return handleRequest(xml);
+        try {
+            String xml = buildRequestXML("subtract", lhs, rhs);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    public static int subtract(Object... params) throws Exception {
+        try {
+            String xml = buildRequestXML("subtract", params);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
     }
     public static int multiply(int lhs, int rhs) throws Exception {
-        String xml = buildRequestXML("multiply", lhs, rhs);
-        return handleRequest(xml);
+        try {
+            String xml = buildRequestXML("multiply", lhs, rhs);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
     }
-    public static int multiply(Integer... params) throws Exception {
-        String xml = buildRequestXML("multiply", params);
-        return handleRequest(xml);
+    public static int multiply(Object... params) throws Exception {
+        try {
+            String xml = buildRequestXML("multiply", params);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
     }
     public static int divide(int lhs, int rhs) throws Exception {
-        String xml = buildRequestXML("divide", lhs, rhs);
-        return handleRequest(xml);
+        try {
+            String xml = buildRequestXML("divide", lhs, rhs);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
     }
     public static int modulo(int lhs, int rhs) throws Exception {
-        String xml = buildRequestXML("modulo", lhs, rhs);
-        return handleRequest(xml);
+        try {
+            String xml = buildRequestXML("modulo", lhs, rhs);
+            String xmlResponse = receiveRequest(xml);
+            return Integer.parseInt(parseResponseXML(xmlResponse));
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
-    private static int handleRequest(String xml) throws Exception {
+    private static String receiveRequest(String xml) throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8080/RPC"))
+            .uri(URI.create("http://" + hostname + ":" + port + "/RPC"))
+            .header("User-Agent", "Elbert Cheng")
             .POST(HttpRequest.BodyPublishers.ofString(xml))
             .build();
 
@@ -68,9 +145,10 @@ public class Client {
         int statusCode = response.statusCode();
         String body = response.body();
 
-        return Integer.parseInt(body);
+        return body;
     }
-    private static String buildRequestXML(String method, Integer... params) throws Exception {
+
+    private static String buildRequestXML(String method, Object... params) throws Exception {
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.newDocument();
 
@@ -81,13 +159,28 @@ public class Client {
         methodNameElement.appendChild(doc.createTextNode(method));
 
         Element paramsElement = doc.createElement("params");
-        for (Integer param : params) {
+        for (Object param : params) {
             Element paramElement = doc.createElement("param");
             Element value = doc.createElement("value");
-            Element intValue = doc.createElement("i4");
 
-            intValue.appendChild(doc.createTextNode(param.toString()));
-            value.appendChild(intValue);
+            Class<?> objClass = param.getClass();
+            String valueTypeString = "";
+
+            switch (objClass.getName()) {
+                case "java.lang.Integer":
+                    valueTypeString = "i4";
+                    break;
+                case "java.lang.String":
+                    valueTypeString = "string";
+                    break;
+                default:
+                    throw new Exception("Invalid parameter type: " + objClass.getName());
+            }
+
+            Element valueType = doc.createElement(valueTypeString);
+
+            valueType.appendChild(doc.createTextNode(param.toString()));
+            value.appendChild(valueType);
             paramElement.appendChild(value);
             paramsElement.appendChild(paramElement);
         }
@@ -101,5 +194,24 @@ public class Client {
         transformer.transform(source, result);
 
         return writer.toString();
+    }
+
+    private static String parseResponseXML(String xml) throws Exception {
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new InputSource(new StringReader(xml)));
+
+        NodeList faultNode = doc.getElementsByTagName("fault");
+
+        if (faultNode.getLength() != 0) {   // error encountered
+            NodeList errorNodes = doc.getElementsByTagName("i4");
+            NodeList errorStringNodes = doc.getElementsByTagName("string");
+            String code = errorNodes.item(0).getTextContent().trim();
+            String str = errorStringNodes.item(0).getTextContent().trim();
+
+            throw new Exception("Error: " + code + " - " + str);
+        }
+
+        NodeList list = doc.getElementsByTagName("value");
+        return list.item(0).getTextContent().trim();
     }
 }
